3bf0c25905e62e008943397cb41968a160f3bcd5
rob-vandenberg <robvandenberg64@gmail.com>
2026-06-25 19:19:34 +0200

    Added height 100% to host

--- src/chrono-picture-card.js
+++ src/chrono-picture-card.js
@@ -6,12 +6,13 @@ import { repeat }                from 'https://unpkg.com/lit@2.0.0/directives/re
 import jsyaml                   from 'https://cdn.jsdelivr.net/npm/js-yaml@4/+esm';
 
 // ─── Version ──────────────────────────────────────────────────────────────────
-const CARD_VERSION = '1.0.109';
+const CARD_VERSION = '1.0.110';
 
 // ─── MDI icon paths ───────────────────────────────────────────────────────────
 const mdiDragHorizontalVariant = 'M9,3H11V5H9V3M13,3H15V5H13V3M9,7H11V9H9V7M13,7H15V9H13V7M9,11H11V13H9V11M13,11H15V13H13V11M9,15H11V17H9V15M13,15H15V17H13V15M9,19H11V21H9V19M13,19H15V21H13V19Z';
 
 // ─── Version History ──────────────────────────────────────────────────────────
+// v1.0.110: Added height: 100%; to :host
 // v1.0.109: Fixed bug with black bar at bottom of camera feed.
 //          Added --video-max-height: none; to :host
 //          Changed height: auto; to height: 100%; in ha-card
@@ -1995,6 +1996,7 @@ class ChronoPictureCard extends LitElement {
   static styles = css`
     :host {
       display: block;
+      height: 100%;
       --video-max-height: none;
     }
     ha-card {
